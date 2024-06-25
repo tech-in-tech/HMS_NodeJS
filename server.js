02567cddf78b450dde420cba482a66c60cbeefa7
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-06-26 01:08:49 +0530

    add gitignore file

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const db = require('./db')
 const bodyParser = require('body-parser');
+require('dotenv').config();
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000
 
 
 
@@ -22,6 +24,7 @@ const menuItemsRoutes= require('./routes/menuItemsRoutes')
 
 app.use('/Person',personRoutes);
 app.use('/menu',menuItemsRoutes);
+
 
 app.listen(3000, () => {
   console.log('Listining on port 3000')
