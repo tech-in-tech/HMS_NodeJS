b256e24729048cb4d205aea8ddfcc66b5a14fb25
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-06-27 00:06:47 +0530

    created a middleware

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const app = express();
 const db = require('./db')
 const bodyParser = require('body-parser');
 require('dotenv').config();
+
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000
 
 
 
+// Middle function
+                               
+const logRequest =(req,res,next)=>{
+  console.log(`${new Date().toLocaleString()} Request Made to : ${req.originalUrl}`)
+  next()
+}
 
+app.use(logRequest)
 
 app.get('/', (req, res) => {
   res.send("Welcome to my hotel website!");
@@ -22,8 +30,10 @@ const personRoutes = require('./routes/personRoutes')
 const menuItemsRoutes= require('./routes/menuItemsRoutes')
 
 
+
 app.use('/Person',personRoutes);
 app.use('/menu',menuItemsRoutes);
+
 
 
 app.listen(3000, () => {
